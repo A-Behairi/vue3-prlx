356be95cd6378a7fef2ac3a09f9b21c31a6c94b5
A-Behairi <behairiahmed@gmail.com>
2023-02-23 14:57:07 +0200

    export directive

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,9 @@
-import type { Plugin, ObjectDirective } from 'vue';
-import { Settings } from './types';
-export declare const VuePrlx: Plugin<ObjectDirective>;
+import type { Plugin, DirectiveBinding } from 'vue';
+import { Settings, EL } from './types';
+export declare const VPrlx: {
+    created: (el: EL, binding: DirectiveBinding) => void;
+    updated: (el: EL, binding: DirectiveBinding) => void;
+    unmounted: (el: EL) => void;
+};
+export declare const VuePrlx: Plugin;
 export type { Settings };
