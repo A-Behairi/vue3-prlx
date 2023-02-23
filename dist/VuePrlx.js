b3379ed6719c2e002d27abe381672b18c7149cc8
A-Behairi <behairiahmed@gmail.com>
2023-02-23 14:57:19 +0200

    export directive

--- dist/VuePrlx.js
+++ dist/VuePrlx.js
@@ -1,66 +1,67 @@
-function r(o) {
-  window.cancelAnimationFrame(o.__prlxRequestAnimationFrameId), delete o.__prlxRequestAnimationFrameId;
+function r(i) {
+  window.cancelAnimationFrame(i.__prlxRequestAnimationFrameId), delete i.__prlxRequestAnimationFrameId;
 }
-function n(o, { modifiers: e = {}, value: i = {} }) {
+function n(i, { modifiers: e = {}, value: o = {} }) {
   const t = {
     isParallaxOnMobile: e.mobile || !1,
     background: e.background || !1,
-    startParallaxFromBottom: i.fromBottom || !1,
-    justAddParallaxValue: i.custom || !1,
-    reverse: i.reverse || !1,
-    speed: i.speed || 0.15,
-    preserveInitialPosition: i.preserveInitialPosition === !1 ? i.preserveInitialPosition : !0,
-    direction: i.direction || "y",
-    limit: i.limit || null,
-    mobileMaxWidth: i.mobileMaxWidth || 768,
-    isDisabled: i.disabled || !1
+    startParallaxFromBottom: o.fromBottom || !1,
+    justAddParallaxValue: o.custom || !1,
+    reverse: o.reverse || !1,
+    speed: o.speed || 0.15,
+    preserveInitialPosition: o.preserveInitialPosition === !1 ? o.preserveInitialPosition : !0,
+    direction: o.direction || "y",
+    limit: o.limit || null,
+    mobileMaxWidth: o.mobileMaxWidth || 768,
+    isDisabled: o.disabled || !1
   };
-  t.background && (t.speed = i.speed || 0.02, t.limit = {
+  t.background && (t.speed = o.speed || 0.02, t.limit = {
     min: 0,
     max: 100
-  }), t.reverse && (t.speed = -t.speed), t.isDisabled ? r(o) : (!(window.innerWidth < t.mobileMaxWidth) || t.isParallaxOnMobile) && l(o, t);
+  }), t.reverse && (t.speed = -t.speed), t.isDisabled ? r(i) : (!(window.innerWidth < t.mobileMaxWidth) || t.isParallaxOnMobile) && l(i, t);
 }
-function l(o, e) {
-  const i = e.startParallaxFromBottom ? window.innerHeight : window.innerHeight / 2;
-  let a = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) - p(o) + i;
-  e.preserveInitialPosition && a < 0 && (a = 0), c(o) && s(o, a, e), o.__prlxRequestAnimationFrameId = window.requestAnimationFrame(l.bind(null, o, e));
+function l(i, e) {
+  const o = e.startParallaxFromBottom ? window.innerHeight : window.innerHeight / 2;
+  let a = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) - p(i) + o;
+  e.preserveInitialPosition && a < 0 && (a = 0), c(i) && s(i, a, e), i.__prlxRequestAnimationFrameId = window.requestAnimationFrame(l.bind(null, i, e));
 }
-function s(o, e, i) {
-  let t = e * i.speed;
-  i.limit && (t > i.limit.max && (t = i.limit.max), t < i.limit.min && (t = i.limit.min));
+function s(i, e, o) {
+  let t = e * o.speed;
+  o.limit && (t > o.limit.max && (t = o.limit.max), t < o.limit.min && (t = o.limit.min));
   let a;
-  i.background ? a = d : i.justAddParallaxValue ? a = m : a = f, a(o, t, i.direction);
+  o.background ? a = d : o.justAddParallaxValue ? a = m : a = f, a(i, t, o.direction);
 }
-function d(o, e, i) {
-  o.style.transition = "background-position 0.1s ease-out", i === "y" ? o.style.backgroundPosition = `50% ${e}%` : o.style.backgroundPosition = `${e}% 50%`;
+function d(i, e, o) {
+  i.style.transition = "background-position 0.1s ease-out", o === "y" ? i.style.backgroundPosition = `50% ${e}%` : i.style.backgroundPosition = `${e}% 50%`;
 }
-function f(o, e, i) {
-  o.style.transition = "transform 0.1s ease-out", o.style.transform = `translate${i.toUpperCase()}(${Math.round(e)}px)`;
+function f(i, e, o) {
+  i.style.transition = "transform 0.1s ease-out", i.style.transform = `translate${o.toUpperCase()}(${Math.round(e)}px)`;
 }
-function m(o, e) {
-  o.style.setProperty("--parallax-value", e);
+function m(i, e) {
+  i.style.setProperty("--parallax-value", e);
 }
-const c = (o, { top: e, height: i } = o.getBoundingClientRect()) => e <= innerHeight && e + i > 0, p = (o) => {
+const c = (i, { top: e, height: o } = i.getBoundingClientRect()) => e <= innerHeight && e + o > 0, p = (i) => {
   let e = 0;
   do
-    e += o.offsetTop || 0, o = o.offsetParent;
-  while (o);
+    e += i.offsetTop || 0, i = i.offsetParent;
+  while (i);
   return e;
-}, x = {
-  install(o, e) {
-    o.directive("prlx", {
-      created: (i, t) => {
-        console.log(e), n(i, t);
-      },
-      updated: (i, t) => {
-        n(i, t);
-      },
-      unmounted: (i) => {
-        r(i);
-      }
-    });
+}, u = {
+  created: (i, e) => {
+    n(i, e);
+  },
+  updated: (i, e) => {
+    n(i, e);
+  },
+  unmounted: (i) => {
+    r(i);
+  }
+}, b = {
+  install(i, e) {
+    i.directive("prlx", u);
   }
 };
 export {
-  x as VuePrlx
+  u as VPrlx,
+  b as VuePrlx
 };
--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,21 @@ import type { App,Plugin,DirectiveBinding, Directive, ObjectDirective} from 'vue
 import { onBind, onUnbind } from './directive';
 import {Settings,EL } from './types';
 
-// export const VPrlx = {
-//     created: (el:EL, binding:DirectiveBinding) => {
-//         onBind(el, binding)
-//     },
-//     updated: (el:EL, binding:DirectiveBinding) => {
-//         onBind(el, binding)
-//     },
-//     unmounted: (el:EL) => {
-//         onUnbind(el)
-//     }
+export const VPrlx = {
+    created: (el:EL, binding:DirectiveBinding) => {
+        onBind(el, binding)
+    },
+    updated: (el:EL, binding:DirectiveBinding) => {
+        onBind(el, binding)
+    },
+    unmounted: (el:EL) => {
+        onUnbind(el)
+    }
 
-// }
-export const VuePrlx:Plugin<ObjectDirective> = {
+}
+export const VuePrlx:Plugin = {
     install(app:App,options:ObjectDirective) {
-        app.directive('prlx', {
-            created: (el:EL, binding:DirectiveBinding) => {
-                console.log(options);
-                onBind(el, binding)
-            },
-            updated: (el:EL, binding:DirectiveBinding) => {
-                onBind(el, binding)
-            },
-            unmounted: (el:EL) => {
-                onUnbind(el)
-            }
-
-        })
+        app.directive('prlx', VPrlx)
     }
 }
 export type  {Settings}
